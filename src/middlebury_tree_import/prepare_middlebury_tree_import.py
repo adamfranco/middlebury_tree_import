d4828a7cfc8d2ee380833b0d7a56bd730d4d7c6a
Adam Franco <afranco@middlebury.edu>
2026-04-04 11:41:13 -0400

    Read directly from ShapeFiles to access database of plant names.

--- src/middlebury_tree_import/prepare_middlebury_tree_import.py
+++ src/middlebury_tree_import/prepare_middlebury_tree_import.py
@@ -1,9 +1,43 @@
+from osmium import SimpleWriter
+from osmium.osm.mutable import Node
+from pyproj import Transformer
+import shapefile
 import typer
 from pathlib import Path
 from typing_extensions import Annotated
 
 def prepare_middlebury_tree_import(
-    input: Annotated[Path, typer.Option(exists=True, readable=True, dir_okay=False, file_okay=True, help="Path to the input .geojson file. Example: campus-trees.geojson")],
+    trees: Annotated[Path, typer.Option(exists=True, readable=True, dir_okay=False, file_okay=True, help="Path to the input shapefile. Example: campus-trees.zip OR campus-trees.shp")],
+    names: Annotated[Path, typer.Option(exists=True, readable=True, dir_okay=False, file_okay=True, help="Path to the input shapefile. Example: campus-trees.zip OR campus-trees.shp")],
     output: Annotated[Path, typer.Option(writable=True, dir_okay=False, file_okay=True, help="Path of the output .osm file. Example: campus-trees.osm")]
 ):
-    print("To do")
+    writer = SimpleWriter(output, overwrite=True)
+
+    # Load all Plant Names for easier lookup.
+    namesDict = {record['PlantPlant']: record.as_dict() for record in shapefile.Reader(names).iterRecords() }
+
+    # Initialize the transformer
+    # EPSG:32145 = NAD83 / Vermont
+    # EPSG:4326 = WGS84
+    transformer = Transformer.from_crs("EPSG:32145", "EPSG:4326", always_xy=True)
+
+    sf = shapefile.Reader(trees)
+    i=0
+    for shapeRecord in sf.shapeRecords():
+        i=i-1
+        treeRec = shapeRecord.record
+        treeFields = treeRec.as_dict()
+        print(treeFields)
+        nameFields = namesDict[treeFields['PlantName']]
+        print(nameFields)
+        tags = {
+            'species:en': nameFields['PlantsComm']
+        }
+        xNAD, yNAD = shapeRecord.shape.points[0]
+        lon, lat = transformer.transform(xNAD / 3.28, yNAD / 3.28)
+        writer.add_node(Node(
+            id = i,
+            location = (lon, lat),
+            tags = tags
+        ))
+        exit()
