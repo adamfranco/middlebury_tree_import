efd7c590f3ce67aaed055efaa70758a62f29efe5
Adam Franco <afranco@middlebury.edu>
2026-04-05 10:06:33 -0400

    Add the taxonomy fields

--- src/middlebury_tree_import/prepare_middlebury_tree_import.py
+++ src/middlebury_tree_import/prepare_middlebury_tree_import.py
@@ -1,5 +1,6 @@
 from osmium import SimpleWriter
 from osmium.osm.mutable import Node
+from pprint import pp
 from pyproj import Transformer
 import shapefile
 import typer
@@ -15,6 +16,7 @@ def prepare_middlebury_tree_import(
 
     # Load all Plant Names for easier lookup.
     namesDict = {record['PlantPlant']: record.as_dict() for record in shapefile.Reader(names).iterRecords() }
+    commonNamesDict = {record['PlantsComm']: record.as_dict() for record in shapefile.Reader(names).iterRecords() }
 
     # Initialize the transformer
     # EPSG:32145 = NAD83 / Vermont
@@ -27,17 +29,42 @@ def prepare_middlebury_tree_import(
         i=i-1
         treeRec = shapeRecord.record
         treeFields = treeRec.as_dict()
-        print(treeFields)
-        nameFields = namesDict[treeFields['PlantName']]
-        print(nameFields)
+        print ("\n")
+        pp(treeFields)
         tags = {
-            'species:en': nameFields['PlantsComm']
+            'natural': 'tree',
+            'species:en': treeFields['COMMON_NAM'],
         }
+        nameFields = None
+        # look up by Id
+        if 'PlantName' in treeFields and treeFields['PlantName'] in namesDict:
+            nameFields = namesDict[treeFields['PlantName']]
+        # Look up by common name if the ids are off.
+        elif 'COMMON_NAM' in treeFields and treeFields['COMMON_NAM'] in commonNamesDict:
+            nameFields = commonNamesDict[treeFields['COMMON_NAM']]
+
+        if nameFields:
+            pp(nameFields)
+            tags['species'] = f"{nameFields['GenusLatin']} {nameFields['PlantsSpec']}"
+            tags['species:en'] = nameFields['PlantsComm']
+            tags['genus'] = nameFields['GenusLatin']
+            tags['taxon'] = nameFields['LatinName']
+            tags['taxon:en'] = nameFields['PlantsComm']
+            tags['taxon:genus'] = nameFields['GenusLatin']
+            tags['taxon:cultivar'] = nameFields['PlantsCult']
+        if 'DBH' in treeFields:
+            tags['diameter'] = f"{treeFields['DBH']}\""
+        if 'DBH' in treeFields:
+            tags['diameter'] = f"{treeFields['DBH']}\""
+
+        pp(tags)
+
         xNAD, yNAD = shapeRecord.shape.points[0]
         lon, lat = transformer.transform(xNAD, yNAD)
         writer.add_node(Node(
             id = i,
             location = (lon, lat),
-            tags = tags
+            tags = tags,
         ))
-        exit()
+        # if i > 5:
+        #     exit()
